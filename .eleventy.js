62cd9d7951286de296c11f796a15bf6d86907ac7
Ginny Mae <ginny@mae.lol>
2026-02-03 13:54:27 -0800

    tweak template a bit

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
     const markdownIt = require('markdown-it');
     const markdownItAnchor = require('markdown-it-anchor');
@@ -9,6 +11,25 @@ module.exports = function(eleventyConfig) {
 			     );
     eleventyConfig.addPassthroughCopy("/home/maive/www/fr.wtf/11ty/src/images");
     eleventyConfig.addPassthroughCopy("/home/maive/www/fr.wtf/11ty/src/favicon.ico");
+
+    eleventyConfig.addPlugin(feedPlugin, {
+	type: "atom", // or "rss", "json"
+	outputPath: "/blog/feed.xml",
+	collection: {
+	    name: "posts", // iterate over `collections.posts`
+	    limit: 0,     // 0 means no limit
+	},
+	metadata: {
+	    language: "en",
+	    title: "Ginny Mae? fr? wtf?",
+	    subtitle: "The incoherent words of a way-too-online tgirl",
+	    base: "https://fr.wtf/",
+	    author: {
+		name: "Ginny Mae",
+		email: "", // Optional
+	    }
+	}
+    });
 
     return {
 	passthroughFileCopy: true,
